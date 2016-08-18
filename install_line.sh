13d9d25c42c99f4d86f05e7c8afb4907ec5d7793
jana_jo <Joanna.Janas@dlr.de>
2016-08-18 15:25:05 +0200

    Provide inital installing

---
+++ install_line.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install vim &&
+sudo apt-get install python3 &&
+sudo apt-get install python &&
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose &&
+sudo apt-get install git git-gui gitk &&
+sudo apt-get install tmux &&
+sudo apt-get install gimp gimp-data gimp-plugin-registry gimp-data-extras
